50f0e9467cd11638c81f96eb2cffebe2a91857cf
Heungsub Lee <sub@nexon.co.kr>
2014-07-08 12:38:02 +0900

    modularize

--- yoyo.go
+++ yoyo.go
@@ -1,39 +1,41 @@
 package main
 
-import "fmt"
-import "net/http"
-import "net/url"
-import "github.com/droundy/goopt"
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+	"github.com/droundy/goopt"
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"net/url"
+)
 
 var host = goopt.String(
-	[]string{"-h", "--host"}, "0.0.0.0",
-	"web server host address to bind.")
-
+	[]string{"-h", "--host"}, "0.0.0.0", "web server host address to bind.")
 var port = goopt.Int(
-	[]string{"-p", "--port"}, 4040,
-	"web server listening port to bind.")
-
+	[]string{"-p", "--port"}, 4040, "web server listening port to bind.")
 var yoAPIToken = goopt.String(
-	[]string{"-y", "--yo"}, "",
-	"the Yo API token.")
+	[]string{"-y", "--yo"}, "", "the Yo API token.")
 
-func main() {
-	// parse argv
+func Parse() {
 	goopt.Summary = "Runs YoYo web server."
 	goopt.Parse(nil)
-	// validate
 	if *yoAPIToken == "" {
 		panic("Yo API token required.")
 	}
-	// web server
-	r := gin.Default()
-	r.GET("/", func(c *gin.Context) {
+}
+
+func WebServer() *gin.Engine {
+	www := gin.Default()
+	www.GET("/", func(c *gin.Context) {
 		query := c.Req.URL.Query()
 		username := query["username"][0]
 		form := url.Values{"api_token": {*yoAPIToken}, "username": {username}}
 		http.PostForm("http://api.justyo.co/yo", form)
 	})
+	return www
+}
+
+func main() {
+	Parse()
 	endpoint := fmt.Sprintf("%s:%d", *host, *port)
-	r.Run(endpoint)
+	WebServer().Run(endpoint)
 }
